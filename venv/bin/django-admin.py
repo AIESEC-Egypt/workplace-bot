81677316f55ec159b7ca018e603cba8fc8dd4797
Ali Soliman <ali.soliman95@gmail.com>
2017-12-12 15:24:11 +0200

    merge meniawy's branch

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Ali/Development/Python/Django/django-facebook-messenger-bot-tutorial/yomamabot/venv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
